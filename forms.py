2745667b3c6b3d34be1d61e2d034776d19c1189a
James <mrnikesb96@gmail.com>
2025-08-24 17:55:46 -0700

    Improve password input: obscure text, confirm password, show errors

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField,PasswordField
+from wtforms.validators import DataRequired, EqualTo
 from flask_ckeditor import CKEditorField
 
 
@@ -14,12 +14,15 @@ class CreatePostForm(FlaskForm):
 class RegisterForm(FlaskForm):
     username = StringField("Username",validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password",validators=[DataRequired()])
+    password = PasswordField("Password",validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[DataRequired(),EqualTo("password",message="Password must match")])
     submit = SubmitField("Sign Up")
 
 class LoginForm(FlaskForm):
     email = StringField("Email",validators=[DataRequired()])
-    password = StringField("Password",validators=[DataRequired()])
+    password = PasswordField("Password",validators=[DataRequired()])
     submit = SubmitField("Sign In")
 
 class CommentForm(FlaskForm):
